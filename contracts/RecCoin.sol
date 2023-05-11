f4293e859a7be4bda86e33ac20eda115116b0f1a
Lukman-01 <abdulyekeenlukman@gmail.com>
2023-05-11 17:28:55 +0100

    Added burn Function

--- contracts/RecCoin.sol
+++ contracts/RecCoin.sol
@@ -33,4 +33,45 @@ contract RecCoin is IERC20, IERC20Metadata, Ownable {
     uint256 public totalSupply;  // The total supply of the token
 
 
+    /**
+     * Burning tokens means permanently removing them from circulation in a cryptocurrency 
+     * or blockchain ecosystem. This process is irreversible and reduces the total supply 
+     * of tokens that exist. 
+     * 
+     * Token burning can be done for various reasons, including reducing inflation and 
+     * increasing the value of remaining tokens by making them scarcer, eliminating lost 
+     * or stolen tokens, or as part of a buyback program where tokens are purchased and 
+     * then burned to increase the value of the remaining tokens. 
+     * 
+     * Token burning is typically achieved by sending the tokens to an unusable 
+     * address that is not controlled by anyone, also known as a "burn address". 
+     * Once the tokens are sent to the burn address, they cannot be retrieved, 
+     * and they are effectively removed from circulation.
+     * 
+     * The "burn" function has the following specifications:
+
+        1. Visibility: The function has a public visibility modifier, which means 
+            it can be called from outside the contract.
+
+        2. Input Parameters: The function takes one input parameter, "amount", 
+            which is an unsigned integer of 256 bits. This parameter represents 
+            the number of tokens to be burned.
+
+        3. Modifiers: The function has one modifier, "onlyOwner", which 
+            restricts the execution of the function to the contract owner. 
+            This modifier ensures that only the contract owner can initiate 
+            the burning of tokens.
+
+        4. Function Call: The function calls the "_burn" function, 
+            passing the contract owner's address (msg.sender) and the 
+            specified amount as arguments. This initiates the burning 
+            of tokens and permanently removes them from circulation.
+
+        5. Return Type: The function does not have a return type, as it is 
+            a void function and does not return any values.
+     */
+    function burn(uint256 amount) public onlyOwner {
+        _burn(msg.sender, amount);
+    }
+
 }
