3316cfd1ecbac7ca765837334ab152491d7b9545
Adeola David Adelakun <aadelakun28@gmail.com>
2023-05-15 08:16:05 +0100

    Set-up for RecCoin test script

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,25 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
+  networks: {
+    hardhat: {
+      gas: "auto",
+      gasPrice: "auto",
+      initialBaseFeePerGas: 0,
+      accounts: {
+        count: 10,
+        initialBalance: "100000000000000000000" // 100 ETH
+      }
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+  mocha: {
+    timeout: 20000
+  }
 };
+
