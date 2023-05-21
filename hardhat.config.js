c70e375c4d57eda4440d0085f4e01cf9fde12acb
Adeola David Adelakun <aadelakun28@gmail.com>
2023-05-21 21:31:02 +0100

    Trial config for github actions

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,47 +1,25 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("hardhat-deploy")
+/*
 require("dotenv").config({ path: ".env" });
 /** @type import('hardhat/config').HardhatUserConfig */
-
+/*
 const RPC_URL = process.env.RPC_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
-
+*/
 
 module.exports = {
   solidity: "0.8.18",
   networks: {
-    sepolia: {
-      url: RPC_URL,
-      chainId: 11155111,
-      accounts: [PRIVATE_KEY],
-      gasPrice: 35000000000,
-      blockConfirmations: 5,
-    },
-
     hardhat: {
-      chainId: 31337,
-    },
-
-    localhost: {
-      chainId: 31337,
-    }
-  },
-
-  namedAccounts: {
-    deployer: {
-      default: 0,
-    },
-    user1: {
-      default: 1,
-    },
-
-    user2: {
-      default: 2,
+      chainId: 1337, // Chain ID of the Hardhat local blockchain
     },
   },
-
-  etherscan: {
-    apiKey: ETHERSCAN_API_KEY,
+  paths: {
+    artifacts: "./artifacts",
+    cache: "./cache",
+    sources: "./contracts",
+    tests: "./test",
   },
 };
