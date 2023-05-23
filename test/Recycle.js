f473d07a8204aa5a2dc159c9420bd3547711b423
Adeola David Adelakun <aadelakun28@gmail.com>
2023-05-23 15:23:24 +0100

    Added test for registeredCompanyCount function.

--- test/Recycle.js
+++ test/Recycle.js
@@ -9,12 +9,14 @@ describe("Recycle", function () {
     let owner;
     let recycle;
     let company;
+    let secondCompany;
+    let thirdCompany;
     let picker;
 
   beforeEach(async function () {
     // Deploy the contract and get the deployed instances
     // Assign test accounts to key players (owner, company, picker) in this contract 
-    [owner, company, picker] = await ethers.getSigners();
+    [owner, company, secondCompany, thirdCompany, picker] = await ethers.getSigners();
     const Recycle = await ethers.getContractFactory("Recycle");
     recycle = await Recycle.deploy();
     await recycle.deployed();
@@ -58,8 +60,53 @@ describe("Recycle", function () {
   describe("getRegisteredCompanyCount", function () {
     it("should return the correct count of registered companies", async function () {
       // Register a new company using the first signer
-      
+      const companyName1 = "Company 1";
+      const minWeightRequirement1 = 100;
+      const maxPricePerKg1 = 10;
+      const isActive1 = true;
+  
+      await recycle.connect(company).registerCompany(
+        companyName1,
+        minWeightRequirement1,
+        maxPricePerKg1,
+        isActive1
+      );
+  
+      // Register a second company using a different signer
+      const companyName2 = "Company 2";
+      const minWeightRequirement2 = 200;
+      const maxPricePerKg2 = 20;
+      const isActive2 = true;
+  
+      await recycle.connect(secondCompany).registerCompany(
+        companyName2,
+        minWeightRequirement2,
+        maxPricePerKg2,
+        isActive2
+      );
+  
+      // Register a third company using another different signer
+      const companyName3 = "Company 3";
+      const minWeightRequirement3 = 300;
+      const maxPricePerKg3 = 30;
+      const isActive3 = true;
+  
+      await recycle.connect(thirdCompany).registerCompany(
+        companyName3,
+        minWeightRequirement3,
+        maxPricePerKg3,
+        isActive3
+      );
+  
+      // Get the registered company count
+      const registeredCompanyCount = await recycle.getRegisteredCompanyCount();
+  
+      // Log key details to the console
+      console.log("Registered Company Count:", registeredCompanyCount.toNumber());
+  
+      // Check if the count is correct
+      expect(registeredCompanyCount).to.equal(3);
     });
-  });
+  });  
   
   });
